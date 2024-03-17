3d9eebc52a02be676e3dae1ab92ab8aca4d20f43
MohamedBoukthir <medboukthir7@gmail.com>
2024-03-17 04:51:38 +0100

    #FIX Admin Account Auto-Creation

--- Platforme/src/main/java/com/mohamed/Repositories/UserRepository.java
+++ Platforme/src/main/java/com/mohamed/Repositories/UserRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUserRole(Role role);
+    User findByRole(Role role);
 }
--- Platforme/src/main/java/com/mohamed/Services/Authentication/AuthenticationServiceImpl.java
+++ Platforme/src/main/java/com/mohamed/Services/Authentication/AuthenticationServiceImpl.java
@@ -3,6 +3,7 @@ package com.mohamed.Services.Authentication;
 import com.mohamed.Entities.User;
 import com.mohamed.Enums.Role;
 import com.mohamed.Repositories.UserRepository;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -14,19 +15,20 @@ public class AuthenticationServiceImpl implements AuthenticationService{
     private final UserRepository userRepository;
 
     // Admin Account Will Generate By Default
+    @PostConstruct
     public void AdminAccountCreation(){
-        User adminAccount = userRepository.findByUserRole(Role.ADMIN);
+        User adminAccount = userRepository.findByRole(Role.ADMIN);
         if (adminAccount == null) {
             User newAdminAccount = new User();
             newAdminAccount.setFullName("Platform Admin");
             newAdminAccount.setEmail("admin@gmail.com");
-            newAdminAccount.setBio("Hello From The Admin Account");
+            newAdminAccount.setBio("Hello From The Admin Account.");
             newAdminAccount.setCountry("TUNISIA");
             newAdminAccount.setGithubLink("");
             newAdminAccount.setLinkedinLink("");
             newAdminAccount.setPassword(new BCryptPasswordEncoder().encode("admin"));
             userRepository.save(newAdminAccount);
-            System.out.println("Welcome " + newAdminAccount.getFullName());
+            System.out.println(" * * * Welcome " + newAdminAccount.getFullName() + " * * * ");
         }
     }
 
